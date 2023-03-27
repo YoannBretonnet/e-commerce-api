ab12ccd62904c63d0d8b099475ec63b5b7092d20
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-27 20:07:26 +0200

    feat : add newUser auth

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth")
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: {
+      j: true,
+      wtimeout: 1000,
+      w: "majority"
+    }
+};
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL)
+mongoose
+.connect(process.env.MONGO_URL, options)
 .then(()=> console.log("DB connection sucessfull"))
 .catch((error)=> {
     console.log(error)
 })
 
 app.use(express.json());
-app.use("/api/users", userRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("server is running")
