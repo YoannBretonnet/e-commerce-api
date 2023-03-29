096352b3b23a77368e9dfead7f40d7dbe6499c43
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-29 20:16:00 +0200

    feat: products CRUD

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-const authRoute = require("./routes/auth")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
 
 const options = {
     useNewUrlParser: true,
@@ -27,6 +29,8 @@ mongoose
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
 
 app.listen(process.env.PORT || 5000, ()=> {
     console.log("server is running")
--- routes/product.js
+++ routes/product.js
@@ -1,31 +1,31 @@
-const Cart = require("../models/Cart");
+const Product = require("../models/Product");
 const {verifyToken, verifyTokenAuthorisation, verifyTokenAndAdmin} = require("./verifyToken");
 
 const router = require("express").Router();
 
 // CREATE
-router.post("/", verifyTokenAuthorisation, async (req,res) =>{
-    const newCart = new Cart(req.body)
+router.post("/", verifyTokenAndAdmin, async (req,res) =>{
+    const newProduct = new Product(req.body)
 
     try{
-        const savedCart = await newCart.save();
-        res.status(200).json(savedCart);
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
     }catch(error) {
         res.status(500).json(error);
     }
 });
 
-// GET USER CART
-router.get("/find/:userId", async (req,res)=>{
+// GET PRODUCT
+router.get("/:id", async (req,res)=>{
     try{
-        const cart = await Cart.findOne({userId: req.params.userId});
-        res.status(200).json(cart)
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product)
     }catch(error){
         res.status(500).json(error);
     }
 });
 
-// GET ALL CARTS
+// GET ALL PRODUCTS
 router.get("/", async (req,res)=>{
     try{
         const products = await Product.find();
@@ -39,21 +39,21 @@ router.get("/", async (req,res)=>{
 // UPDATE
 router.put("/:id", verifyTokenAuthorisation, async (req,res)=>{
     try{
-        const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+        const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {
             $set: req.body,
         },
         {new:true});
-        res.status(200).json(updatedCart);
+        res.status(200).json(updatedProduct);
     }catch(error){
         res.status(500).json(error)
     }
 });
 
 // DELETE
-router.delete("/:id", verifyTokenAuthorisation, async (req,res)=>{
+router.delete("/:id", verifyTokenAndAdmin, async (req,res)=>{
     try{
-        await Cart.findByIdAndDelete(req.params.id);
-        res.status(200).json("Cart has been deleted")
+        await Product.findByIdAndDelete(req.params.id);
+        res.status(200).json("Product has been deleted")
     }catch(error){
         res.status(500).json(error);
     }
