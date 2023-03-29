ba702bd16c39513db6484f01066a6e854ffa516b
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-03-29 16:05:03 +0200

    feat: get all users

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,21 @@ const {verifyToken, verifyTokenAuthorisation, verifyTokenAndAdmin} = require("./
 const router = require("express").Router();
 
 // GET USER
-router.delete("/:id", verifyTokenAndAdmin, async (req,res)=>{
+router.get("/find/:id", verifyTokenAndAdmin, async (req,res)=>{
     try{
         const user = await User.findById(req.params.id);
-        res.status(200).json(user)
+        const {password, ...others} = user._doc;
+        res.status(200).json({others})
+    }catch(error){
+        res.status(500).json(error);
+    }
+});
+
+// GET ALL USERS
+router.get("/find", verifyTokenAndAdmin, async (req,res)=>{
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
     }catch(error){
         res.status(500).json(error);
     }
