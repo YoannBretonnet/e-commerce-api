5e72c76312c9a3c3a36de29f95f035284d40a111
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-04-06 11:41:54 +0200

    featr: added routes

--- models/Product.js
+++ models/Product.js
@@ -6,9 +6,8 @@ const ProductSchema = new mongoose.Schema(
         desc:{type: String, required: true},
         img:{type: String, required: true},
         category:{type: String, required: true},
-        size:{type: String},
-        color:{type: String},
-        price:{type: Number, required: true},
+        size:{type: Array},
+        price:{type: Number, required: true}
     },
     {timestamps: true}
 );
