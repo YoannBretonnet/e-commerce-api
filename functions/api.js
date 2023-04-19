31d1c458050e82195d56506a6ec8001dde7c8140
Yoann Bretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2023-04-19 21:46:52 +0200

    update

--- functions/api.js
+++ functions/api.js
@@ -4,11 +4,11 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
-const authRoute = require("../routes/auth");
-const productRoute = require("../routes/product");
-const cartRoute = require("../routes/cart");
-const orderRoute = require("../routes/order");
-const stripeRoute = require("../routes/stripe")
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe")
 const cors = require("cors");
 
 const options = {
